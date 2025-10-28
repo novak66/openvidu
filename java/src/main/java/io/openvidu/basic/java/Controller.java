dd9050279362cf0057714d4611277fbf25067e43
novak <lucasnovak66@gmail.com>
2025-10-28 14:18:39 -0300

    openvidu

---
+++ java/src/main/java/io/openvidu/basic/java/Controller.java
@@ -0,0 +1,84 @@
+package io.openvidu.basic.java;
+
+import java.time.Instant;
+import java.util.Date;
+import java.util.Map;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.*;
+import org.springframework.web.client.RestTemplate;
+
+import io.livekit.server.AccessToken;
+import io.livekit.server.RoomJoin;
+import io.livekit.server.RoomName;
+import io.livekit.server.WebhookReceiver;
+import livekit.LivekitWebhook.WebhookEvent;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+@CrossOrigin(origins = "*")
+@RestController
+public class Controller {
+
+	private String LIVEKIT_API_KEY = "testandoEssaSecretParaTercertezaqueelavaitermaisdetrintaedoischaracteres";
+
+	private String LIVEKIT_API_SECRET ="testandoEssaSecretParaTercertezaqueelavaitermaisdetrintaedoischaracteres";
+
+	/**
+	 * @param params JSON object with roomName and participantName
+	 * @return JSON object with the JWT token
+	 */
+	@PostMapping(value = "/token")
+	public ResponseEntity<Map<String, String>> createToken(@RequestBody Map<String, String> params) {
+		String roomName = params.get("roomName");
+		String participantName = params.get("participantName");
+
+		if (roomName == null || participantName == null) {
+			return ResponseEntity.badRequest().body(Map.of("errorMessage", "roomName and participantName are required"));
+		}
+
+		AccessToken token = new AccessToken(LIVEKIT_API_KEY, LIVEKIT_API_SECRET);
+		token.setName(participantName);
+		token.setIdentity(participantName);
+		token.addGrants(new RoomJoin(true), new RoomName(roomName));
+
+		return ResponseEntity.ok(Map.of("token", token.toJwt()));
+	}
+
+	@PostMapping("/rooms")
+	public ResponseEntity<Map<String, String>>  createRoom(@RequestBody Map<String, String> params) {
+		String roomName = params.get("roomName");
+
+		AccessToken token = new AccessToken(LIVEKIT_API_KEY, LIVEKIT_API_SECRET);
+		token.setName(roomName);
+		token.setIdentity(roomName);
+		token.addGrants(new RoomJoin(true), new RoomName(roomName));
+
+		return ResponseEntity.ok(Map.of("rooms", token.toJwt()));
+	}
+
+
+	@PostMapping(value = "/livekit/webhook", consumes = "application/webhook+json")
+	public ResponseEntity<String> receiveWebhook(@RequestHeader("Authorization") String authHeader, @RequestBody String body) {
+		WebhookReceiver webhookReceiver = new WebhookReceiver(LIVEKIT_API_KEY, LIVEKIT_API_SECRET);
+		try {
+			WebhookEvent event = webhookReceiver.receive(body, authHeader);
+			System.out.println("LiveKit Webhook: " + event.toString());
+		} catch (Exception e) {
+			System.err.println("Error validating webhook event: " + e.getMessage());
+		}
+		return ResponseEntity.ok("ok");
+	}
+
+}

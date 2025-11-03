637c1b13bd8a3f1fbc5a26429c0eebcce7d633cc
novak <lucasnovak66@gmail.com>
2025-11-03 11:59:08 -0300

    fix

--- openvidu-vue/src/App.vue
+++ openvidu-vue/src/App.vue
@@ -7,7 +7,7 @@ import {
     Room,
     RoomEvent
 } from 'livekit-client';
-import { onUnmounted, ref, type Ref } from 'vue';
+import { onMounted, onUnmounted, ref, type Ref } from 'vue';
 import VideoComponent from './components/VideoComponent.vue';
 import AudioComponent from './components/AudioComponent.vue';
 
@@ -20,12 +20,13 @@ type TrackInfo = {
 // For other deployment type, configure them with correct URLs depending on your deployment
 let APPLICATION_SERVER_URL = '';
 let LIVEKIT_URL = '';
+let APPLICATION_OPENVIDU_SERVER_URL = '';
 configureUrls();
 
 function configureUrls() {
-    APPLICATION_SERVER_URL = 'https://' + 'hml-openvidu.prontumais.com.br:8080/api/';
-
-    LIVEKIT_URL = 'wss://' + 'hml-openvidu.prontumais.com.br/ws';
+    APPLICATION_SERVER_URL = 'https://' + 'hml-openvidu.prontumais.com.br/api/';
+    APPLICATION_OPENVIDU_SERVER_URL = 'https://' + 'hml-openvidu.prontumais.com.br/internal-api/v1/';
+    LIVEKIT_URL = 'wss://' + 'hml-openvidu.prontumais.com.br';
 }
 
 const room = ref<Room>();
@@ -35,13 +36,17 @@ const remoteTracksMap: Ref<Map<string, TrackInfo>> = ref(new Map());
 let participantName = ref('Participant' + Math.floor(Math.random() * 100));
 let roomName = ref('Room');
 
-async function joinRoom() {
-    // Initialize a new Room object
+onUnmounted(() => {
+    leaveRoom();
+});
 
+onMounted(() => {
+    login('admin', 'xlCvppgl2OxMOXsYP9SEtwhpAgPDZNLseVtp2Sd6X1X');
+});
+
+async function joinRoom() {
     room.value = new Room();
 
-    // Specify the actions when events take place in the room
-    // On every new Track received...
     room.value.on(
         RoomEvent.TrackSubscribed,
         (_track: RemoteTrack, publication: RemoteTrackPublication, participant: RemoteParticipant) => {
@@ -52,27 +57,21 @@ async function joinRoom() {
         }
     );
 
-    // On every Track destroyed...
     room.value.on(RoomEvent.TrackUnsubscribed, (_track: RemoteTrack, publication: RemoteTrackPublication) => {
         remoteTracksMap.value.delete(publication.trackSid);
     });
 
     try {
-        // Get a token from your application server with the room name and participant name
         const token = await getToken(roomName.value, participantName.value);
 
-        // Connect to the room with the LiveKit URL and the token
-        await room.value.connect(LIVEKIT_URL, token);
-
-        // Publish your camera and microphone
+        await room.value.connect(LIVEKIT_URL, 'token');
         await room.value.localParticipant.enableCameraAndMicrophone();
-        localTrack.value = room.value.localParticipant.videoTrackPublications.values().next().value.videoTrack;
+        localTrack.value = room.value?.localParticipant?.videoTrackPublications?.values()?.next()?.value?.videoTrack;
     } catch (error: any) {
         console.log('There was an error connecting to the room:', error.message);
         await leaveRoom();
     }
 
-    // Add listener for beforeunload event to leave the room when the user closes the tab
     window.addEventListener('beforeunload', leaveRoom);
 }
 
@@ -88,24 +87,6 @@ async function leaveRoom() {
     window.removeEventListener('beforeunload', leaveRoom);
 }
 
-onUnmounted(() => {
-    // On component unmount, leave the room
-    leaveRoom();
-});
-
-/**
- * --------------------------------------------
- * GETTING A TOKEN FROM YOUR APPLICATION SERVER
- * --------------------------------------------
- * The method below request the creation of a token to
- * your application server. This prevents the need to expose
- * your LiveKit API key and secret to the client side.
- *
- * In this sample code, there is no user control at all. Anybody could
- * access your application server endpoints. In a real production
- * environment, your application server must identify the user to allow
- * access to the endpoints.
- */
 async function getToken(roomName: string, participantName: string) {
     const response = await fetch(APPLICATION_SERVER_URL + 'token', {
         method: 'POST',
@@ -126,47 +107,76 @@ async function getToken(roomName: string, participantName: string) {
     const data = await response.json();
     return data.token;
 }
+
+async function login(username: string, password: string) {
+    const response = await fetch(APPLICATION_SERVER_URL + 'auth/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            username,
+            password
+        })
+    });
+
+    if (!response.ok) {
+        const error = await response.json();
+        throw new Error(`Failed to get token: ${error.errorMessage}`);
+    }
+
+    const data = await response.json();
+    return data.token;
+}
 </script>
 
 <template>
-    <div v-if="!room" id="join">
-        <div id="join-dialog">
-            <h2>Join a Video Room</h2>
-            <form @submit.prevent="joinRoom">
-                <div>
-                    <label for="participant-name">Participant</label>
-                    <input v-model="participantName" id="participant-name" class="form-control" type="text" required />
-                </div>
-                <div>
-                    <label for="room-name">Room</label>
-                    <input v-model="roomName" id="room-name" class="form-control" type="text" required />
-                </div>
-                <button class="btn btn-lg btn-success" type="submit" :disabled="!roomName || !participantName">
-                    Join!
-                </button>
-            </form>
+    <div id="app">
+        <div v-if="!room" id="join">
+            <div id="join-dialog">
+                <h2>Join a Video Room</h2>
+                <form @submit.prevent="joinRoom">
+                    <div>
+                        <label for="participant-name">Participant</label>
+                        <input
+                            v-model="participantName"
+                            id="participant-name"
+                            class="form-control"
+                            type="text"
+                            required
+                        />
+                    </div>
+                    <div>
+                        <label for="room-name">Room</label>
+                        <input v-model="roomName" id="room-name" class="form-control" type="text" required />
+                    </div>
+                    <button class="btn btn-lg btn-success" type="submit" :disabled="!roomName || !participantName">
+                        Join!
+                    </button>
+                </form>
+            </div>
         </div>
-    </div>
-    <div v-else id="room">
-        <div id="room-header">
-            <h2 id="room-title">{{ roomName }}</h2>
-            <button class="btn btn-danger" id="leave-room-button" @click="leaveRoom">Leave Room</button>
-        </div>
-        <div id="layout-container">
-            <VideoComponent
-                v-if="localTrack"
-                :track="localTrack"
-                :participantIdentity="participantName"
-                :local="true"
-            />
-            <template v-for="remoteTrack of remoteTracksMap.values()" :key="remoteTrack.trackPublication.trackSid">
+        <div v-else id="room">
+            <div id="room-header">
+                <h2 id="room-title">{{ roomName }}</h2>
+                <button class="btn btn-danger" id="leave-room-button" @click="leaveRoom">Leave Room</button>
+            </div>
+            <div id="layout-container">
                 <VideoComponent
-                    v-if="remoteTrack.trackPublication.kind === 'video'"
-                    :track="remoteTrack.trackPublication.videoTrack!"
-                    :participantIdentity="remoteTrack.participantIdentity"
+                    v-if="localTrack"
+                    :track="localTrack"
+                    :participantIdentity="participantName"
+                    :local="true"
                 />
-                <AudioComponent v-else :track="remoteTrack.trackPublication.audioTrack!" hidden />
-            </template>
+                <template v-for="remoteTrack of remoteTracksMap.values()" :key="remoteTrack.trackPublication.trackSid">
+                    <VideoComponent
+                        v-if="remoteTrack.trackPublication.kind === 'video'"
+                        :track="remoteTrack.trackPublication.videoTrack!"
+                        :participantIdentity="remoteTrack.participantIdentity"
+                    />
+                    <AudioComponent v-else :track="remoteTrack.trackPublication.audioTrack!" hidden />
+                </template>
+            </div>
         </div>
     </div>
 </template>

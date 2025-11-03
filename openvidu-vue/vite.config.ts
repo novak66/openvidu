056610ef0f02cd7124aba74c543f14b1d3bcee44
novak <lucasnovak66@gmail.com>
2025-11-03 12:34:03 -0300

    fix

--- openvidu-vue/vite.config.ts
+++ openvidu-vue/vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
-  base: '/vue/',  // ← ADICIONAR ESSA LINHA
+  base: '/vue/',  
   plugins: [
     vue(),
     vueDevTools(),
